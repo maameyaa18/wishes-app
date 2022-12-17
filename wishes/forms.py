205309672f1fa4e5e6f822e3674464fb39fe7580
maameyaa18 <maameyaa.osei@ashesi.edu.gh>
2022-12-17 07:50:09 +0000

    Added users to wishes

--- wishes/forms.py
+++ wishes/forms.py
@@ -4,4 +4,4 @@ from .models import Wishes
 class WishForm(forms.ModelForm):
     class Meta:
         model = Wishes
-        fields = ['title', 'status', 'main_photo', 'photo_1', 'photo_2', 'list_date']
+        fields = ['user', 'title', 'status', 'main_photo', 'photo_1', 'photo_2', 'list_date']

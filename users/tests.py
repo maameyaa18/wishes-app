0733c996f84806c9151d034206d21808ac78fd25
maameyaa18 <maameyaa.osei@ashesi.edu.gh>
2022-12-17 15:35:56 +0000

    Added tests and removing static files

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from users.views import log_out, register
 
-# Create your tests here.
+class UrlTest(TestCase):
+    def testLogoutPage(self):
+       response = self.client.get('log_out/')
+       print(response)
+       self.assertEqual(response.status_code, 200)
+
+    def testRegisterPage(self):
+        response = self.client.get('register/')
+        print(response)
+        self.assertEqual(response.status_code, 200)
+

b90f739c37817ff27979e2f08639cf0084a84a10
maameyaa18 <maameyaa.osei@ashesi.edu.gh>
2022-12-18 23:31:26 +0000

    Removed user from form and updated gitignore

--- wishes/forms.py
+++ wishes/forms.py
@@ -4,4 +4,4 @@ from .models import Wishes
 class WishForm(forms.ModelForm):
     class Meta:
         model = Wishes
-        fields = ['user', 'title', 'status', 'main_photo', 'photo_1', 'photo_2', 'list_date']
+        fields = [ 'title', 'status', 'main_photo', 'photo_1', 'photo_2', 'list_date']
